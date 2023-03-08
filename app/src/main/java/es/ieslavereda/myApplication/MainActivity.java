1f13e2fb63510d54ca9ec52e1f4097ac12190aa8
Jose Luis <jolutase104@gmail.com>
2023-03-08 21:18:06 +0100

    Initial commit

---
+++ app/src/main/java/es/ieslavereda/myApplication/MainActivity.java
@@ -0,0 +1,17 @@
+package es.ieslavereda.myApplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
