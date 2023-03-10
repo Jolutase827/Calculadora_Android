5c061def6ca8993469329506fd913fede987ef2d
Jose Luis <jolutase104@gmail.com>
2023-03-10 14:12:15 +0100

    10/03/2023

--- app/src/main/java/es/ieslavereda/myApplication/MainActivity.java
+++ app/src/main/java/es/ieslavereda/myApplication/MainActivity.java
@@ -12,6 +12,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private float a;
+    private float b;
+
+    private boolean borrar;
+    private char operacion;
+
     private TextView text;
     private Button b1;
     private Button b2;
@@ -38,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
-
+        operacion = ' ';
+        a=0;
+        b=0;
+        borrar = false;
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
@@ -63,72 +72,266 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b1.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b1.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b1.getText());
+                }
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b2.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b2.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b2.getText());
+                }
             }
         });
         b3.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b3.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b3.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b3.getText());
+                }
             }
         });
         b4.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b4.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b4.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b4.getText());
+                }
             }
         });
         b5.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b5.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b5.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b5.getText());
+                }
             }
         });
         b6.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b6.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b6.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b6.getText());
+                }
             }
         });
         b7.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b8.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b7.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b7.getText());
+                }
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b8.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b8.getText());
+                }
             }
         });
         b9.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b9.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b9.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b9.getText());
+                }
             }
         });
         b0.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText(text.getText()+""+b0.getText());
+                if (!borrar) {
+                    text.setText(text.getText() + "" + b0.getText());
+                }else{
+                    borrar = false;
+                    text.setText(b0.getText());
+                }
+
+
             }
         });
         bAC.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                text.setText("");
+                text.setText("0");
+                operacion = ' ';
+                a =0;
+                b=0;
             }
         });
+        bC.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                text.setText(text.getText().subSequence(0,text.getText().length()-1));
+            }
+        });
+        bMultiplicacion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (operacion==' ') {
+                    a = Float.parseFloat(text.getText().toString());
+                    operacion = '*';
+                }else {
+                    b = Float.parseFloat(text.getText().toString());
+                    if (b == 0 && operacion == '/') {
+                        text.setText("Err.");
+                    } else {
+                        a = operar();
+                        text.setText(Float.toString(a));
+                        operacion = '*';
+                        b= 0;
+                    }
+                }
+
+
+                borrar = true;
+            }
+        });
+
+        bSuma.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (operacion==' ') {
+                    a = Float.parseFloat(text.getText().toString());
+                    operacion = '+';
+                }else {
+                    b = Float.parseFloat(text.getText().toString());
+                    if (b == 0 && operacion == '/') {
+                        text.setText("Err.");
+                    } else {
+                        a = operar();
+                        text.setText(Float.toString(a));
+                        operacion = '+';
+                        b=0;
+                    }
+                }
+
+
+                borrar = true;
+            }
+        });
+
+        bResta.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (operacion==' ') {
+                    a = Float.parseFloat(text.getText().toString());
+                    operacion = '-';
+                }else {
+                    b = Float.parseFloat(text.getText().toString());
+                    if (b == 0 && operacion == '/') {
+                        text.setText("Err.");
+                    } else {
+                        a = operar();
+                        text.setText(Float.toString(a));
+                        operacion = '-';
+                        b=0;
+                    }
+                }
+
+
+                borrar = true;
+            }
+        });
+        bComa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (text.getText().equals("")||borrar){
+                    text.setText("0.");
+                }else
+                    text.setText(text.getText()+".");
+            }
+        });
+        bDivision.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (operacion==' ') {
+                    a = Float.parseFloat(text.getText().toString());
+                    operacion = '/';
+                }else {
+                    b = Float.parseFloat(text.getText().toString());
+                    if (b == 0 && operacion == '/') {
+                        text.setText("Err.");
+                    } else {
+                        a = operar();
+                        text.setText(Float.toString(a));
+                        operacion = '/';
+                        b=0;
+                    }
+                }
+
+
+                borrar = true;
+            }
+        });
+
+        bIgual.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (operacion!=' ') {
+                    b = Float.parseFloat(text.getText().toString());
+                    if (b == 0 && operacion == '/') {
+                        text.setText("Err.");
+                    } else {
+                        a = operar();
+                        text.setText(Float.toString(a));
+                        operacion = ' ';
+                        a=0;
+                        b=0;
+                    }
+                }
+                borrar = true;
+            }
+        });
+
+
         bC.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -139,5 +342,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public float operar(){
+        switch (operacion){
+            case('*'):
+                return a*b;
+            case('-'):
+                return a-b;
+            case('+'):
+                return a+b;
+            case('/'):
+                return a/b;
+            default:
+                return 0;
+        }
     }
 }
